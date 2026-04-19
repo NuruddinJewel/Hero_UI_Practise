eaf16487870673e421f9f5a7288eda111eae440e
Jewel <m.76jewel@gmail.com>
2026-04-19 23:26:46 +0600

    Hero UI Buttons

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,17 @@
+import { Button } from '@heroui/react';
+import React from 'react';
+
+const HeroPage = () => {
+    return (
+        <div>
+            <h2>Hero Page</h2>
+            <Button>Hero Button</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="tertiary">Secondary</Button>
+            <Button variant="outline">Secondary</Button>
+        </div>
+    );
+};
+
+export default HeroPage;
+// Drawer
